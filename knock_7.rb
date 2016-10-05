4413fb4305bce754d54262c42eff98c4afc17952
原田陽紗子 <hharada@goodpatch.com>
2016-10-05 12:27:20 +0900

    Used Range#include? method and Array#select method.

--- knock_7.rb
+++ knock_7.rb
@@ -10,17 +10,9 @@
 # [1, 'a', 2.2].sum #=> 3.2
 
 class Array
-  # def sum
-  #   cleanArray = self.select { |item| (item.class == Fixnum || item.class == Float) }
-  #   total = cleanArray.reduce(:+)
-  #   return total
-  # end
   def sum
-    total = 0
-    self.each{|item| if (-Float::INFINITY..Float::INFINITY).include?(item)
-      total += item
-    end
-    }
+    cleanArray = self.select{|item| (-Float::INFINITY..Float::INFINITY).include?(item)}
+    total = cleanArray.reduce(:+)
     return total
   end
 end
